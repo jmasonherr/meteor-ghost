24d2a5684780a53aa9cb8b67ef765cad33db2310
Jeremy Mason-Herr <jmasonherr@gmail.com>
2015-10-18 09:57:01 -0700

    updated for updates
    https://github.com/TryGhost/Ghost/wiki/Using-Ghost-as-an-NPM-module#update

--- ghost.js
+++ ghost.js
@@ -1,1 +1,6 @@
 ghost = Npm.require('ghost');
+// Needed according to 
+// https://github.com/TryGhost/Ghost/wiki/Using-Ghost-as-an-NPM-module#update
+ghost().then(function (ghostServer) {
+    ghostServer.start();
+});
